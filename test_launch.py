a9f01be4f9ed2a61153d35296e309999430aac36
Penguinification <75798136+Penguinification@users.noreply.github.com>
2023-01-30 17:21:35 +0100

    Add and implement launch
    This adds a function that calculates the final launch decision based on
    the contents of the fuv. It returns true if all elements are true and
    false otherwise. Two unit tests have also been added that test the
    situation where condition is true and when it is false.
    
    Fixes #32

---
+++ test_launch.py
@@ -0,0 +1,17 @@
+from launch import launch
+
+def test_launch_true():
+    """
+    Tests that launch returns true if all elements in the fuv are true
+    """
+    fuv = [True] * 15
+    result = launch(fuv)
+    assert(result)
+
+def test_launch_false():
+    """
+    Tests that launch returns false if the fuc contains false elements
+    """
+    fuv = [int(i % 2) for i in range(15)]
+    result = launch(fuv)
+    assert(not result)
